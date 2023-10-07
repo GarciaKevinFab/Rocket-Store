68a671cc7c762e17062740053433850fef56e901
Kevin-fg <kevinfge2410@gmail.com>
2023-10-06 21:00:49 -0500

    arreglos-links

--- src/pages/payment/Payment.js
+++ src/pages/payment/Payment.js
@@ -5,7 +5,7 @@ import Breadcrumbs from "../../components/pageProps/Breadcrumbs";
 const Payment = () => {
   return (
     <div className="max-w-container mx-auto px-4">
-      <Breadcrumbs title="Payment gateway" />
+      <Breadcrumbs title="Pasarela de pago" />
       <div className="pb-10">
         <p>Próximamente disponible.</p>
         <Link to="/">
